919515bd3f7dc024fb4780ea25be901c46d4cfe0
wendaining <wendaining@foxmail.com>
2026-07-26 22:18:23 +0800

    attack xv6 successful

--- user/attack.c
+++ user/attack.c
@@ -7,6 +7,21 @@ int
 main(int argc, char *argv[])
 {
   // Your code here.
-
+  char *p = sbrk(8*4096);
+  if (p == (char*)-1) {
+    exit(1);
+  }
+  for (int i = 0; i < 8*4096 - 16; i++) {
+    if (p[i] =='T') {
+      char tmp[15];
+      memcpy(tmp, p + i, 15);
+      if (!strcmp(tmp, "This may help.")) {
+        for (char *c = p + i + 16; *c; c++) {
+          printf("%c", *c);
+        }
+        printf("\n");
+      }
+    }
+  }
   exit(1);
 }
